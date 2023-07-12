481dff3a61b9968f7202d74ef55ea7bd6ac30508
Barathsaraan <saraan.barath@gmail.com>
2023-07-12 12:35:16 +0530

    add

---
+++ main.js
@@ -0,0 +1,17 @@
+// Nested if
+var player=prompt("Enter your favorite Food");
+if(player=="Idly"){
+	document.write("Idly");
+}
+else if(player=="Dosa"){
+	document.write("Dosa");
+}
+else if(player=="Chapathi"){
+	document.write("Chapathi");
+}
+else if(player="pongal"){
+	document.write("pongal");
+}
+else{
+	document.write("not your favorite food");
+}
